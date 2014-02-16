ec0461f473ff6ecfdf7af0e8a2a880bb039eed06
Drew Crawford <drew@sealedabstract.com>
2014-02-15 23:00:05 -0600

    some ios tests for libsodium

---
+++ LibSodiumTester/LibSodiumTester/LibSodiumTester.h
@@ -0,0 +1,13 @@
+//
+//  LibSodiumTester.h
+//  LibSodiumTester
+//
+//  Created by Drew Crawford on 2/15/14.
+//  Copyright (c) 2014 DrewCrawfordApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibSodiumTester : NSObject
+
+@end
---
+++ LibSodiumTester/LibSodiumTester/LibSodiumTester.m
@@ -0,0 +1,13 @@
+//
+//  LibSodiumTester.m
+//  LibSodiumTester
+//
+//  Created by Drew Crawford on 2/15/14.
+//  Copyright (c) 2014 DrewCrawfordApps. All rights reserved.
+//
+
+#import "LibSodiumTester.h"
+
+@implementation LibSodiumTester
+
+@end
